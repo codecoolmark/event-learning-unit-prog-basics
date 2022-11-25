71a6cef11b5882f18220fd8bf2d7a026c601f511
Mark Strempel <mark.strempel@codecool.com>
2022-11-25 08:44:26 +0100

    Fixed a typo and added solution for the fourth fith task.

--- 1 Events around us/reference code/script.js
+++ 1 Events around us/reference code/script.js
@@ -32,3 +32,41 @@ const tellMeWhereMyMouseIs = document.getElementById('tell-me-where-my-mouse-is'
 tellMeWhereMyMouseIs.addEventListener('click', function(event) {
     console.log(event.x, event.y);
 });
+
+// solution for the fourth task
+
+const iSawAnimalButtons = document.querySelectorAll('.i-saw-animal-button');
+
+iSawAnimalButtons.forEach(button => {
+    button.addEventListener('click', function(event) {
+        if (event.target.innerText === 'I saw an elephant') {
+            console.log('Elephant');
+        } else if (event.target.innerText === 'I saw a snake') {
+            console.log('Snake')
+        } else if (event.target.innerText === 'I saw a ghost') {
+            console.log('Ghost')
+        } else if (event.target.innerText === 'I saw a monkey') {
+            console.log('Monkey');
+        } else if (event.target.innerText === 'I saw a kangaroo') {
+            console.log('Kangaroo');
+        } else if (event.target.innerText === 'I saw a parrot') {
+            console.log('Parrot');
+        }
+    })
+})
+
+// solution for the fifth task
+
+const increaseCounter = document.getElementById('increase-counter');
+const decreaseCounter = document.getElementById('decrease-counter');
+let counter = 0;
+
+increaseCounter.addEventListener('click', function() {
+    counter = counter + 1;
+    console.log(counter);
+});
+
+decreaseCounter.addEventListener('click', function() {
+    counter = counter - 1;
+    console.log(counter);
+})
